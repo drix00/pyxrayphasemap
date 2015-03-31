9ae3a3dd2be53995a4ee2b920f6e1f673aa82fc6
Hendrix Demers <hendrix.demers@mail.mcgill.ca>
2015-03-30 23:14:23 -0400

    Compute maximum pixel spectrum from raw data.

--- pyxrayphasemap/raw_map.py
+++ pyxrayphasemap/raw_map.py
@@ -14,6 +14,7 @@ __copyright__ = "Copyright (c) 2015 Hendrix Demers"
 __license__ = "GPL 3"
 
 # Standard library modules.
+import os.path
 
 # Third party modules.
 import matplotlib.pyplot as plt
@@ -26,19 +27,41 @@ from pySpectrumFileFormat.Bruker.MapRaw.MapRawFormat import MapRawFormat
 # Globals and constants variables.
 
 def run():
-    filepath = r"D:\work\results\experiments\SU8230\hdemers\adam\12samples\Adam_12samples_20150316_2015-03-16_17-47\Sample01.raw"
+    filepathLain = r"D:\work\results\experiments\SU8230\hdemers\adam\12samples\Adam_12samples_20150316_2015-03-16_17-47\Sample01.raw"
+    filepathChimera = r"/home/hdemers/work/results/experiments/12samples/Adam_12samples_20150316_2015-03-16_17-47/Sample01.raw"
+    if os.path.isfile(filepathLain):
+        filepath = filepathLain
+    elif os.path.isfile(filepathChimera):
+        filepath = filepathChimera
+
     rawMap = MapRawFormat(filepath)
 
     plt.figure()
 
-    energy, pixelSpectrum = rawMap.getSpectrum(0, 0)
-    plt.plot(energy, pixelSpectrum)
+    #energy, pixelSpectrum = rawMap.getSpectrum(0, 0)
+    #plt.plot(energy, pixelSpectrum)
 
-    energy, pixelSpectrum = rawMap.getSpectrum(200, 200)
-    plt.plot(energy, pixelSpectrum)
+    #energy, pixelSpectrum = rawMap.getSpectrum(200, 200)
+    #plt.plot(energy, pixelSpectrum)
+
+    energy, maximum_pixelSpectrum = rawMap.getMaximumPixelSpectrum()
+    plt.plot(energy, maximum_pixelSpectrum)
+    pixels = rawMap.getMaximumPixelSpectrumPixels()
+    print(pixels[218:226])
+
+    #energy, maximum_pixelSpectrum = rawMap.getMaximumPixelSpectrum2()
+    #plt.plot(energy, maximum_pixelSpectrum)
+
+    plt.figure()
+    for ix, iy in pixels[218:226]:
+        energy, pixelSpectrum = rawMap.getSpectrum(ix, iy)
+        plt.plot(energy, pixelSpectrum)
 
     #plt.figure()
+    #energy, total_spectrum = rawMap.getTotalSpectrum()
+    #plt.plot(energy, total_spectrum)
 
+    #plt.figure()
     #image = rawMap.getTotalIntensityImage()
     #plt.imshow(image)
 
